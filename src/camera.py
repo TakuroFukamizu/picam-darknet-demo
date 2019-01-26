6c5ff7e0e3ff6d3c59fc49904677faf24fbfb6e6
Takuro Fukamizu <takuro.f.1987@gmail.com>
2019-01-27 02:26:31 +0900

    fix camera methods

--- src/camera.py
+++ src/camera.py
@@ -10,8 +10,11 @@ def capture():
     file_name = "{}.jpg".format(datetime.now().strftime("%Y%m%d-%H%M%S"))
     file_path = os.path.join(TEMP_CAPTURED_DIR, file_name)
     with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768) # XGA
+        # camera.resolution = (1024, 768) # XGA
+        camera.resolution = (608, 608) # XGA
         camera.vflip = True
-        sleep(5) # it's need
+        camera.hflip = True
+        # sleep(5) # it's need
+        sleep(1)
         camera.capture(file_path)
     return file_path
