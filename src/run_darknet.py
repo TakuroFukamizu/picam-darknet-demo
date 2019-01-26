5bd1a7a4551392af9b51a84f1f11927da267151f
Takuro Fukamizu <takuro.f.1987@gmail.com>
2019-01-26 21:47:43 +0900

    fix exec procedure

--- src/run_darknet.py
+++ src/run_darknet.py
@@ -2,15 +2,23 @@
 import os
 import re
 import subprocess
+import argparse
 from configs import ROOT_DIR, DARKNET_PATH
 
-def run_command(cmd):
+def run_command(cmd: str):
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout_data, stderr_data = p.communicate()
+    print('stdout_data', type(stdout_data))
+    print('stderr_data', type(stderr_data))
+
+    if len(stdout_data) > 0:
+        stdout_data = str(stdout_data).split('\n')
+    if len(stderr_data) > 0:
+        stderr_data = str(stderr_data).split('\n')
+    
     return p.returncode, stdout_data, stderr_data
 
-if __name__ == '__main__':
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+def exec_darknet(image_path: str):
     main_workdir = os.getcwd()
 
     os.chdir(DARKNET_PATH)
@@ -21,7 +29,10 @@ if __name__ == '__main__':
     pattern_predict_item = '([\w-]+): ([0-9.]+)%'
     repatter_predict_item = re.compile(pattern_predict_item)
 
-    ret, stdout, stderr = run_command('./darknet detector test cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights data/person.jpg')
+    ret, stdout, stderr = run_command(
+        './darknet detector test cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights {}'.format(
+            image_path
+        ))
     if len(stdout) > 0:
         print("## stdout")
         print(stdout)
@@ -31,7 +42,7 @@ if __name__ == '__main__':
 
     was_predicted = False
     predicted_results = []
-    for line in str(stdout).split('\n'):
+    for line in stdout:
         if not was_predicted:
             result = repatter_predict_finish.match(line)
             if result:
@@ -47,27 +58,11 @@ if __name__ == '__main__':
                 predicted_results.append((class_label, prod))
             else:
                 print('invalid line', line)
+    return predicted_results
+
+if __name__ == '__main__':
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+    predicted_results = exec_darknet('data/person.jpg')
     print(predicted_results)
-
-
-# with open(os.path.join(root_dir, 'darknet_detecotr_test_out1.txt')) as f:
-#     was_predicted = False
-#     predicted_results = []
-#     for line in f:
-#         if not was_predicted:
-#             result = repatter_predict_finish.match(line)
-#             if result:
-#                 elapsed_time = float(result.group(1))
-#                 fps = 1.0 / elapsed_time
-#                 print(result.group(), '{} FPS'.format(fps))
-#                 was_predicted = True
-#         else:  # 認識結果
-#             result = repatter_predict_item.match(line)
-#             if result:
-#                 class_label = result.group(1)
-#                 prod = int(result.group(2)) / 100
-#                 predicted_results.append((class_label, prod))
-#             else:
-#                 print('invalid line', line)
-#     print(predicted_results)
-        
+    
