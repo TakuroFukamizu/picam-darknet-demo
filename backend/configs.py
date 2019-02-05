ce801463bdd96a707db2c0f636ddce77348fe9eb
Takuro Fukamizu <takuro.f.1987@gmail.com>
2019-02-05 19:54:22 +0900

    revork darknet setting

--- backend/configs.py
+++ backend/configs.py
@@ -22,7 +22,12 @@ if len(USER_DARKNET_LABEL_FILE) > 0 and len(USER_DARKNET_CONFIG_FILE) > 0 and le
     USER_DARKNET_WEIGHT_FILE = os.path.join(ROOT_DIR, USER_DARKNET_WEIGHT_FILE)
 
 
+# DARKNET_LABEL_FILE = os.path.join(DARKNET_PATH, "manacamera/labels.txt")
+# DARKNET_CONFIG_FILE = os.path.join(DARKNET_PATH, "manacamera/yolo3-tiny.cfg")
+# DARKNET_WEIGHT_FILE = os.path.join(DARKNET_PATH, "manacamera/latest.weights")
+# DARKNET_DATASET_FILE = os.path.join(DARKNET_PATH, "manacamera/dataset.txt")
+
 DARKNET_LABEL_FILE = os.path.join(DARKNET_PATH, "manacamera/labels.txt")
-DARKNET_CONFIG_FILE = os.path.join(DARKNET_PATH, "manacamera/yolo3-tiny.cfg")
-DARKNET_WEIGHT_FILE = os.path.join(DARKNET_PATH, "manacamera/latest.weights")
-DARKNET_DATASET_FILE = os.path.join(DARKNET_PATH, "manacamera/dataset.txt")
+DARKNET_CONFIG_FILE = os.path.join(DARKNET_PATH, "cfg/yolov3-tiny.cfg")
+DARKNET_WEIGHT_FILE = os.path.join(DARKNET_PATH, "yolov3-tiny.weights")
+DARKNET_DATASET_FILE = os.path.join(DARKNET_PATH, "cfg/coco.data")
