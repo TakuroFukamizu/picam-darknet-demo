92b34960bc48bc9b910f71a08c5bffeadaae7299
Takuro Fukamizu <takuro.f.1987@gmail.com>
2019-02-06 08:16:41 +0900

    fix sleep timing

--- backend/camera.py
+++ backend/camera.py
@@ -17,5 +17,5 @@ def capture():
         # sleep(5) # it's need
         sleep(1)
         camera.capture(file_path)
-    sleep(2)
+    sleep(0.5)
     return file_path
--- backend/darknet/darknet_cli.py
+++ backend/darknet/darknet_cli.py
@@ -68,15 +68,8 @@ def exec_darknet(config: YoloConfig, image_path: str):
     #         image=image_path
     #     ))
     os.environ['PATH'] = '{}:{}'.format(DARKNET_PATH, os.environ['PATH'])
-    print(os.environ['PATH'])
-    print(os.path.join(DARKNET_PATH, 'darknet'), os.path.exists(os.path.join(DARKNET_PATH, 'darknet')))
-    print(config.dataset_file, os.path.exists(config.dataset_file))
-    print(config.config_file, os.path.exists(config.config_file))
-    print(config.weights_file, os.path.exists(config.weights_file))
-    print(image_path, os.path.exists(image_path))
     ret, stdout, stderr = run_command(['./darknet', 'detector', 'test', config.dataset_file, config.config_file, config.weights_file, image_path])
-    
-    
+
     # エラーチェック
     for line in stderr:
         result = repatter_stb_reason.match(line)
